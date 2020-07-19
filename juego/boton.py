837a492c0f7013cb1a98971b86a96a83b89c5b1b
Joaquin201 <62035965+Joaquin201@users.noreply.github.com>
2020-07-19 18:29:41 -0300

    Update boton.py

--- juego/boton.py
+++ juego/boton.py
@@ -5,10 +5,10 @@ class Boton():
     self._color = color
 
 
-  def get_boton(self):
+  def get_boton(self, i, j):
     if self._color == 0:
-      return sg.Button("",button_color=('black','white'), size=(2, 2), pad=(0, 0) )
+      return sg.Button("",button_color=('black','white'), size=(2, 2), pad=(0, 0), key = (i , j) )
     elif self._color == 1:
-      return sg.Button("",button_color=('black','green'), size=(2, 2), pad=(0, 0) )
+      return sg.Button("",button_color=('black','green'), size=(2, 2), pad=(0, 0), key = (i , j))
     elif self._color == 2:
-      return sg.Button("",button_color=('black','red'), size=(2, 2), pad=(0, 0) )
+      return sg.Button("",button_color=('black','red'), size=(2, 2), pad=(0, 0), key = (i , j))
