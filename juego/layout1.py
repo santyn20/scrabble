3a6b156c66c8f9685472045115c742504aab1e1a
Joaquin201 <62035965+Joaquin201@users.noreply.github.com>
2020-07-26 15:27:27 -0300

    Update layout1.py

--- juego/layout1.py
+++ juego/layout1.py
@@ -6,53 +6,58 @@ from boton2 import Boton2
 from atril import Atril
 from ficha import Ficha
 from tablero1 import Tablero1
+from IA import InteligenciaArtificial
 
 def main():
-	tab = Tablero1()
-	at = Atril()
-	ch = Check()
-	layout = tab.crearTablero()
-	layout.append(at.mostrarAtril())
-	layout.append([sg.Button("cerrar"),sg.Button("check")])
-	window = sg.Window('Tablero', layout)
-	ok = True
-	f = None
-	medio = False
-	inicio = (0,0)
+  tab = Tablero1()
+  at = Atril()
+  ia = InteligenciaArtificial(at.getAtril())
+  ch = Check()
+  layout = tab.crearTablero()
+  layout.append(at.mostrarAtril())
+  layout.append([sg.Button("cerrar"),sg.Button("check")])
+  window = sg.Window('Tablero', layout)
+  ok = True
+  f = None
+  medio = False
+  inicio = (0,0)
 
-	while ok:
-		event,values = window.read()
+  while ok:
+    event,values = window.read()
 		
-		if (type(event) == Ficha):
-			if (f != None):
-				at.devolverFicha()
-				f = None
-			else:
-				f = event.getFicha()
-				at.actualizarAtril(event)
-		if ((type(event) == Boton0) or (type(event) == Boton1) or (type(event) == Boton2)):
-			if ((event.getCoor() == (8,8)) and (f != None) and (not medio)):
-				medio = True
-			if (medio):
+    if (type(event) == Ficha):
+      if (f != None):
+        at.devolverFicha()
+        f = None
+      else:
+        f = event.getFicha()
+        at.actualizarAtril(event)
+    if ((type(event) == Boton0) or (type(event) == Boton1) or (type(event) == Boton2)):
+      if ((event.getCoor() == (8,8)) and (f != None) and (not medio)):
+        medio = True
+      if (medio):
 
-				if (ch.checkLugar(event.getCoor())):
+        if (ch.checkLugar(event.getCoor())):
 
-					if (event.getFicha() == None):
-						if (f != None):
-							if (ch.getPosIni() == (0,0)):
-								ch.setPosIni(event.getCoor())
-							event.setFicha(f)
-							ch.agregarLetra(event)
-							at.actualizarAtril(f,True)
-							f = None
+          if (event.getFicha() == None):
+            if (f != None):
+              if (ch.getPosIni() == (0,0)):
+                ch.setPosIni(event.getCoor())
+              event.setFicha(f)
+              ch.agregarLetra(event)
+              at.actualizarAtril(f,True)
+              f = None
 
-		if (event == "check"):
-			if (ch.buscar()):
-				print ("es valida")
-			else:
-				print ("es invalida")
-			
-
-		if (event == "cerrar"):
-			ok = False
-	window.close()
+    if (event == "check"):
+      if (ch.buscar()):
+        print ("es valida")
+      else:
+        print ("es invalida")
+      palencontrada = ia.buscar()
+      for i in palencontrada:
+        print(i)
+        print(ia.datosBuenos().index(i))	
+         
+        if (event == "cerrar"):
+          ok = False
+  window.close()
